71ffc53eddb6b56b5dd94738aa9631b9a5a44a79
Reed Stewart <reedostewart@gmail.com>
2018-11-12 17:58:31 +1300

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+function nameHoverOff() {
+    var text = document.getElementById("nameList");
+    text.innerHTML = "REED STEWART";
+}
+
+/*
+function iconHoverOff(icon) {
+    icon.style.width = '100px';
+    icon.style.height = '100px';
+}
+*/
