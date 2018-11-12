37431aa9d5cbbbcaaf232c76a444f150c31297f1
Reed Stewart <reedostewart@gmail.com>
2018-11-12 18:00:36 +1300

    Removed comment

--- JS/script.js
+++ JS/script.js
@@ -2,10 +2,3 @@ function nameHoverOff() {
     var text = document.getElementById("nameList");
     text.innerHTML = "REED STEWART";
 }
-
-/*
-function iconHoverOff(icon) {
-    icon.style.width = '100px';
-    icon.style.height = '100px';
-}
-*/
