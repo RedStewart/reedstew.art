c7ba6cacc72272d885f62e357b12dcf6f8ba9006
Reed Stewart <reedostewart@gmail.com>
2019-01-03 22:00:35 +1300

    Corner button functionality added

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,49 @@
-function nameHoverOff() {
-    var text = document.getElementById("nameList");
-    text.innerHTML = "REED STEWART";
+var cornSwitch = false;
+
+function buttonClick(button) {
+    var midContent = document.getElementById('divContent');
+    var mainContent = document.getElementById('mainDivContent');
+    if (cornSwitch == true) {
+        cornSwitch = false;
+        console.log(cornSwitch);
+        console.log("close window");
+        $(midContent).animate({ 'min-height': '85vh' }, 1000);
+    }
+    /*
+        if (button.active == true) {
+            console.log("close window");
+            
+            button.active = false;
+            $(midContent).animate({ 'min-height': '85vh' }, 1000);
+        }
+        */
+    else {
+
+        $(midContent).animate({ 'min-height': '40vh' }, 1000);
+        fadeInfo(button.innerHTML)        
+        
+        cornSwitch = true;
+        console.log(cornSwitch);
+    }
 }
+
+function fadeInfo(cornClick) {
+
+    console.log("YEET " + cornClick);
+
+    
+    if (cornClick == 'About') {
+        console.log('This is the ' + cornClick + ' button');
+        //button.style.fontSize = 25 + 'px';
+    }
+    else if (cornClick == 'Skills') {
+        console.log('This is the ' + cornClick + ' button');
+    }
+    else if (cornClick == 'Projects') {
+        console.log('This is the ' + cornClick + ' button');
+    }
+    else {
+        console.log('This is the ' + cornClick + ' button');
+    }
+}
+
