8ca6c45222a4d2c6b87f26ef6b325559a08b5b26
Reed Stewart <reedostewart@gmail.com>
2019-01-29 13:01:00 +1300

    Content added to skills corner. JS detects which icon hovered, needing to add description for each icon.

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,42 @@
 var cornSwitch = false;
 
+var skillIcons = document.getElementsByClassName("skillIcon")
+for (i = 0; i < skillIcons.length; i++) {
+    skillIcons[i].addEventListener("mouseenter", function () {
+        var iconID = this.id;
+        //create div to display information about each language before the statments
+
+        if (iconID == "svgJS") {
+            console.log("this is javascripthurr");            
+
+        } else if (iconID == "svgNode") {
+            console.log("this is nodehurr");     
+
+        } else if (iconID == "svgHTML") {
+            console.log("this is htmlhurr");     
+
+        } else if (iconID == "svgCSS") {
+            console.log("this is csshurr");     
+
+        } else if (iconID == "svgPhp") {
+            console.log("this is phphurr");     
+
+        } else if (iconID == "svgCS") {
+            console.log("this is cshurr");     
+
+        } else if (iconID == "svgJava") {
+            console.log("this is javahurr");     
+
+        } else if (iconID == "svgPython") {
+            console.log("this is pythonhurr");     
+
+        } else {
+            console.log("this is githurr");     
+
+        }
+    }, false);
+}
+
 function buttonClick(button) {
     var midContent = document.getElementById('divContent');
     var mainContent = document.getElementById('divMainContent');
@@ -19,7 +56,6 @@ function buttonClick(button) {
 
         $(midContent).animate({ 'min-height': '35vh' }, 1000);
         $(mainContent).fadeIn(1000);
-
         fadeInfo(button.innerHTML)
 
         cornSwitch = true;
@@ -38,23 +74,24 @@ function fadeInfo(cornClick) {
         cornImg.src = "Images/about.png";
     }
     else if (cornClick == 'Skills') {
-        cornContent.innerHTML = "These are the current technologies I'm familiar with:";
+        cornContent.innerHTML = "";
         cornImg.src = "Images/skills.png";
-
+        /*
+                var svgJS = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+                var svgJSPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+                svgJS.id = "svgJS"
+                svgJS.setAttribute("class", "skillIcon");
+                svgJS.setAttribute("viewBox", "0 0 128 128");
+                svgJSPath.setAttribute("fill", "#eeeeee")
+                svgJSPath.setAttribute("d", "M2 1v125h125v-125h-125zm66.119 106.513c-1.845 3.749-5.367 6.212-9.448 7.401-6.271 1.44-12.269.619-16.731-2.059-2.986-1.832-5.318-4.652-6.901-7.901l9.52-5.83c.083.035.333.487.667 1.071 1.214 2.034 2.261 3.474 4.319 4.485 2.022.69 6.461 1.131 8.175-2.427 1.047-1.81.714-7.628.714-14.065-.001-10.115.046-20.188.046-30.188h11.709c0 11 .06 21.418 0 32.152.025 6.58.596 12.446-2.07 17.361zm48.574-3.308c-4.07 13.922-26.762 14.374-35.83 5.176-1.916-2.165-3.117-3.296-4.26-5.795 4.819-2.772 4.819-2.772 9.508-5.485 2.547 3.915 4.902 6.068 9.139 6.949 5.748.702 11.531-1.273 10.234-7.378-1.333-4.986-11.77-6.199-18.873-11.531-7.211-4.843-8.901-16.611-2.975-23.335 1.975-2.487 5.343-4.343 8.877-5.235l3.688-.477c7.081-.143 11.507 1.727 14.756 5.355.904.916 1.642 1.904 3.022 4.045-3.772 2.404-3.76 2.381-9.163 5.879-1.154-2.486-3.069-4.046-5.093-4.724-3.142-.952-7.104.083-7.926 3.403-.285 1.023-.226 1.975.227 3.665 1.273 2.903 5.545 4.165 9.377 5.926 11.031 4.474 14.756 9.271 15.672 14.981.882 4.916-.213 8.105-.38 8.581z");
+        
+                svgJS.appendChild(svgJSPath);
+                mainDiv.appendChild(svgJS);
+        */
     }
-
-
-
-
-
-
-
-
-
     else if (cornClick == 'Projects') {
         cornContent.innerHTML = "Over the past few years I have worked on multiple different projects and assignments, here are a few of my most recent.";
         cornImg.src = "Images/projects.png";
-
 
         //
         var projectHead1 = document.createElement("div");
@@ -123,37 +160,23 @@ function fadeInfo(cornClick) {
         listItem5.innerHTML = "Supply-Store-Raffle-Script";
         projectList2.appendChild(listItem5);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     else {
         cornContent.innerHTML = "This is how to contact me.";
         cornImg.src = "Images/contact.png";
     }
 }
 
+
+
+
+
+
+
+
+
+
+//use function to draw svg using path
+function drawSVG(id, path) {
+
+}
